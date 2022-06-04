a5deb155f305b67492ea83606bc84775643941b1
Aswin Asok <aswinasokofficial@gmail.com>
2022-06-04 15:11:33 +0530

    Working on main section

---
+++ components/Input/Input.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Input.module.css";
+
+const Input = () => {
+  return (
+    <div className={styles.inputcontainer}>
+      <div className={styles.abouttfp}>
+        The Foundation Program is a 30-Day Hybrid Bootcamp aimed at addressing
+        the skill gap by helping students up-skill with a foundation program in
+        programming and web development. The program aims to help the students
+        build a foundational skill level to find opportunities in the Industry.
+      </div>
+    </div>
+  );
+};
+
+export default Input;
