2097599a7b03028e97495d50ffbf374b05a5012d
Aswin Asok <aswinasokofficial@gmail.com>
2022-06-18 21:40:37 +0530

    Testing Error handling

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -34,6 +34,27 @@ let meta = [
 ];
 
 export default function User({ status, data }) {
+  if ((status = 500 || data == undefined)) {
+    console.log(status);
+    return (
+      <>
+        <CustomizedSnackbars />
+        <Head>
+          <title>Foundation Program - Error</title>
+          <meta
+            name="description"
+            content="Welcome to The Foundation Program Website, Enter your foundaton program Id and view all the badges you have aquired untill now in the program"
+          />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <div className={styles.main}>
+          <Navbar />
+          <Input />
+          <Footer />
+        </div>
+      </>
+    );
+  }
   if (status == 200) {
     return (
       <>
@@ -85,26 +106,6 @@ export default function User({ status, data }) {
             })}
           </div>
 
-          <Footer />
-        </div>
-      </>
-    );
-  } else {
-    console.log(status)
-    return (
-      <>
-        <CustomizedSnackbars />
-        <Head>
-          <title>Foundation Program - Error</title>
-          <meta
-            name="description"
-            content="Welcome to The Foundation Program Website, Enter your foundaton program Id and view all the badges you have aquired untill now in the program"
-          />
-          <link rel="icon" href="/favicon.png" />
-        </Head>
-        <div className={styles.main}>
-          <Navbar />
-          <Input />
           <Footer />
         </div>
       </>
