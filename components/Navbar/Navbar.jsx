43b689cd4556b1c45f4bc35457519ea949651144
Aswin Asok <aswinasokofficial@gmail.com>
2022-06-30 20:48:35 +0530

    Production issue fixing

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import Image from "next/image";
-import Mulearn from "./assets/Mulearn White.png";
+import Mulearn from "./assets/Mulearn.png";
 import Link from "next/link";
 
 
