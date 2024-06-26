1caeee7ab32d349ab8cf1d8495fd6667d7eeaf87
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-06-26 18:44:55 +0530

    feat(ui):wall of fame section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import TaskInfo from "@/modules/TaskInfo/TaskInfo";
-import Landingpage from "../modules/LandingPage/landing"
-import LevelInfo from "../modules/LevelInfo/levelInfo"
+import Landingpage from "../modules/LandingPage/landing";
+import LevelInfo from "../modules/LevelInfo/levelInfo";
+import WallOfFame from "@/modules/WallOfFame/WallOfFame";
 export default function Home() {
   return (
     <main>
-     <Landingpage />
-     <LevelInfo />
-     <TaskInfo />
+      <Landingpage />
+      <LevelInfo />
+      <TaskInfo />
+      <WallOfFame />
     </main>
   );
 }
