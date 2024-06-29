c9cba6766a9276b0e1956cac1c8403b426e30ae9
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-06-29 10:17:20 +0530

    feat(ui):badgeCollection added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import TaskInfo from "@/modules/TaskInfo/TaskInfo";
 import Landingpage from "../modules/LandingPage/landing";
 import LevelInfo from "../modules/LevelInfo/levelInfo";
 import WallOfFame from "@/modules/WallOfFame/WallOfFame";
+import BadgeCollection from "@/modules/BadgeCollection/BadgeCollection";
 export default function Home() {
   return (
     <main>
@@ -9,6 +10,7 @@ export default function Home() {
       <LevelInfo />
       <TaskInfo />
       <WallOfFame />
+      <BadgeCollection />
     </main>
   );
 }
